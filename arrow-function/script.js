e9a026d56a75ea05b24dfa1498455811e78318f4
Dzikry <dzikrynp14@gmail.com>
2025-07-07 13:00:08 +0700

    added arrow-function

---
+++ arrow-function/script.js
@@ -0,0 +1,40 @@
+// Function Expression
+// const showUser = function (user) {
+//   return `Hi ${user}`;
+// };
+// console.log(showUser("User1"));
+
+// ================================================
+
+// Arrow Function
+// const showUser = (user) => {
+//   return `Hi ${user}`;
+// };
+// console.log(showUser("User2"));
+
+// ================================================
+
+// let students = ["Student1", `Student2`, `Student3`];
+// let total = students.map(function (student) {
+//   return student.length;
+// });
+
+// console.log(total);
+
+// ================================================
+
+// let students = ["Student1", `Student2`, `Student3`];
+// let total = students.map((student) => {
+//   return student.length;
+// });
+
+// console.log(total);
+
+// ================================================
+
+// let students = ["Student1", `Student2`, `Student3`];
+// let total = students.map((student) => {
+//   return { studentName: student, nameLength: student.length };
+// });
+
+// console.log(total);
