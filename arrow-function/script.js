f6c7b2bf485afa01ea45fd7520017b0770036ae6
Dzikry <dzikrynp14@gmail.com>
2025-07-07 16:30:10 +0700

    updated

--- arrow-function/script.js
+++ arrow-function/script.js
@@ -38,3 +38,70 @@
 // });
 
 // console.log(total);
+
+// ================================================
+
+// `this` in Arrow Function
+
+// Constructor Function
+// const User = function () {
+//   this.username = "User1";
+//   this.email = "User1@gmail.com";
+//   this.sayHello = function () {
+//     console.log(`Hi, my name is ${this.username}`);
+//   };
+// };
+
+// const user1 = new User();
+
+// ================================================
+
+// Arrow Function in Method
+// const User = function () {
+//   this.username = "User1";
+//   this.email = "User1@gmail.com";
+//   this.sayHello = () => {
+//     console.log(`Hi, my name is ${this.username}`);
+//   };
+// };
+
+// const user1 = new User();
+
+// ================================================
+
+// Object Literal
+
+// `this` is targetting window
+// const user = {
+//   username: "User1",
+//   email: "user1@gmail.com",
+//   sayHello: () => {
+//     console.log(`Hi, my name is ${this.username}`);
+//   },
+// };
+
+// ================================================
+
+// const User = function () {
+//   this.username = "User1";
+//   this.age = 25;
+//   this.sayHello = function () {
+//     console.log(`Hi, my name is ${this.username}`);
+//   };
+
+//   console.log(this);
+
+//   // setInterval(function () {
+//   //   console.log(this.age);
+//   //   console.log(this);
+//   // }, 500);
+
+//   setInterval(() => {
+//     console.log(this.age);
+//     console.log(this);
+//   }, 500);
+// };
+
+// const user1 = new User();
+
+// ================================================
