71bce1ef8a0f72bf1741f8628dc5e0e0af826786
Dzikry <dzikrynp14@gmail.com>
2025-07-08 17:16:13 +0700

    added template-literals

---
+++ template-literals/script.js
@@ -0,0 +1,24 @@
+// Template Literals / Template String
+// const username = "user1";
+// const age = 245;
+// console.log(`Hi, I'm ${username}, ${age}`);
+
+// Embedded Expression
+// console.log(`${1 + 1}`);
+// console.log(`${alert(`Hi!`)}`);
+// const x = 10;
+// console.log(`${x % 2 == 0 ? "even" : "odd"}`);
+
+// HTML Fragments
+// const student = {
+//   name: "Student",
+//   age: 123,
+//   email: "student@gmail.com",
+// };
+
+// const el = `<div class="student">
+//     <h2>${student.name}</h2>
+//     <span class:"email">${student.email}</span>
+// </div>`;
+
+// console.log(el);
