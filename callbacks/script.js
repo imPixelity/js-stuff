86510ce3279429b88c957101b13a9d475b815f4f
Dzikry <dzikrynp14@gmail.com>
2025-07-10 20:21:37 +0700

    stuff

--- callbacks/script.js
+++ callbacks/script.js
@@ -43,3 +43,55 @@
 // student.forEach((e) => {
 //   console.log(e.name);
 // });
+
+// ======================================================
+
+// Asynchronous Callback -> This one saved to web API, allowing the rest of the code to run without waiting.
+
+// function getDummyData(url, success, error) {
+//   let xhr = new XMLHttpRequest();
+
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4) {
+//       if (xhr.status === 200) {
+//         success(xhr.response);
+//       } else if (xhr.status === 404) {
+//         error();
+//       }
+//     }
+//   };
+
+//   xhr.open("get", url);
+//   xhr.send();
+// }
+
+// console.log("Status-1");
+// getDummyData(
+//   "data/dummy1.json",
+//   (result) => {
+//     const dummy = JSON.parse(result);
+//     dummy.forEach((el) => {
+//       console.log(el.name);
+//     });
+//   },
+//   () => {}
+// );
+// console.log("Status-2"); // This will run, not blocked by getDummyData()
+
+// ======================================================
+
+// JQuery
+
+// console.log("Status-1");
+// $.ajax({
+//   url: "data/dummy1.json",
+//   success: (dummy) => {
+//     dummy.forEach((el) => {
+//       console.log(el.name);
+//     });
+//   },
+//   error: (e) => {
+//     console.log(e.responseText);
+//   },
+// });
+// console.log("Status-2");
