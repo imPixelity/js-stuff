7dab6c75f275f6fda70e5912d7624bd2ee8c03a1
Dzikry <dzikrynp14@gmail.com>
2025-07-10 16:43:25 +0700

    added callbacks

---
+++ callbacks/script.js
@@ -0,0 +1,45 @@
+// Callback
+// Synchronous Callback
+
+// function message(name) {
+//   alert(`Hi, ${name}`);
+// }
+
+// function displayMessage(callback) {
+//   const name = prompt(`Enter your name: `);
+//   callback(name);
+// }
+
+// displayMessage(message);
+
+// ======================================================
+
+// function displayMessage(callback) {
+//   const name = prompt(`Enter your name: `);
+//   callback(name);
+// }
+
+// displayMessage((name) => {
+//   alert(`Hi, ${name}`);
+// });
+
+// ======================================================
+
+// const student = [
+//   {
+//     name: "student1",
+//     id: 1,
+//   },
+//   {
+//     name: "student2",
+//     id: 2,
+//   },
+//   {
+//     name: "student3",
+//     id: 3,
+//   },
+// ];
+
+// student.forEach((e) => {
+//   console.log(e.name);
+// });
