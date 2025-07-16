8a772ef738e60e6f0237833d67a8f0b70443f2a8
Dzikry <dzikrynp14@gmail.com>
2025-07-16 22:06:42 +0700

    added class

---
+++ js-oop/class/script.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHello(name) {
+    console.log(`Hello ${name}, I'm ${this.name}`);
+  }
+}
+
+const Dummy = new Person("Dummy");
