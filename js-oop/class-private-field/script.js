41d19c8df2fae908b089c3d86576eb9cb20f9db4
Dzikry <dzikrynp14@gmail.com>
2025-07-17 11:41:45 +0700

    added private-field

---
+++ js-oop/class-private-field/script.js
@@ -0,0 +1,24 @@
+// Private Field
+class Counter {
+  #counter = 0;
+
+  increment() {
+    this.#counter++;
+  }
+
+  decrement() {
+    this.#counter--;
+  }
+
+  get() {
+    return this.#counter;
+  }
+}
+
+const counter = new Counter();
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+console.log(counter.get());
