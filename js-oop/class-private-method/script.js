b8fdc1d6ecc36179f637b62ab8187020e33bf1b1
Dzikry <dzikrynp14@gmail.com>
2025-07-17 11:56:02 +0700

    stuff

--- js-oop/class-private-method/script.js
+++ js-oop/class-private-method/script.js
@@ -1,1 +1,20 @@
 // Private Method
+class Person {
+  say(name) {
+    if (name) {
+      this.#sayWithName(name);
+    } else {
+      this.#sayWithoutName();
+    }
+  }
+
+  #sayWithoutName() {
+    console.log("Hello");
+  }
+
+  #sayWithName(name) {
+    console.log(`Hello ${name}`);
+  }
+}
+
+const john = new Person();
